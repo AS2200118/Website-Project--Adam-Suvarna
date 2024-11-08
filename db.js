ad67ef8bb77a98ae40d327420423efc3a880cde8
AS2200118 <146171755+AS2200118@users.noreply.github.com>
2024-11-09 00:02:29 +0200

    added table+error handling
    I created tables for the restaurants + users + reservations. I will add more tables for more features as well

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-//Basic handler for the database
+//Basic handler for the database:
 const sqlite3 = require('sqlite3').verbose();
 const db = newsqlite3.Database('./database.db', (err)=> {
     if (err) {
@@ -9,6 +9,45 @@ const db = newsqlite3.Database('./database.db', (err)=> {
     }
 });
 
-//Now to create the table
+//Now to create the tables + error handling:
+const RestaurantsTable = `CREATE TABLE IF NOT EXISTS restaurants(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    location TEXT,
+    cuisine TEXT,
+    availability TEXT,
+    categories TEXT)`;
+db.run(RestaurantsTable, (err)=> {
+    if (err) {
+        return console.error("Error creating Restaurant Table:", err.message);
+    }
+    console.log("Restaurant Table created successfully");
+});
 
+const UsersTable = `CREATE TABLE IF NOT EXISTS users(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT,
+    email TEXT)`;
+db.run(UsersTable, (err)=> {
+    if (err) {
+        return console.error("Error creating Users Table:", err.message);
+    }
+    console.log("User Table created successfully");
+});
+
+const ReservationTable = `CREATE TABLE IF NOT EXISTS reservation(
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_ID INTEGER,
+    restaurant_ID INTEGER,
+    date TEXT,
+    time TEXT,
+    status TEXT,
+    FOREIGN KEY (user_ID) REFERENCES users(ID),
+    FOREIGN KEY (restaurant_ID) REFERENCES restaurants(ID))`;
+db.run(RestaurantsTable, (err)=> {
+    if (err) {
+        return console.error("Error creating Restaurant Table:", err.message);
+        }
+        console.log("Restaurant Table created successfully");
+});
 
